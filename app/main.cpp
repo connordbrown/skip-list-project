3ed39f359ee216f39cbcf10663ba3207490f9483
Connor Brown <Connor.Brown@colorado.edu>
2024-12-04 22:32:15 +0000

    add init_node

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../code/Node.h"
+#include "../code/SkipList.h"
 
 using namespace std;
 
--- code/SkipList.cpp
+++ code/SkipList.cpp
@@ -9,3 +9,15 @@ SkipList::SkipList() {
 // destructor
 SkipList::~SkipList() {}
 
+// create and initialize new node
+node* SkipList::init_node(string key, string data) {
+    // allocate memory for ret
+    node* ret = new node;
+
+    // assign key and data to ret, set next pointer to NULL
+    ret->key = key;
+    ret->data = data;
+    ret->next = NULL;
+    
+    return ret;
+}
