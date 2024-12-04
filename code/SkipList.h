b31a24e87b35fafa8d7ed72d23a6c41c8fd6fab5
Connor Brown <Connor.Brown@colorado.edu>
2024-12-04 22:17:25 +0000

    rearrange class, add MAX_LEVEL macro

--- code/SkipList.h
+++ code/SkipList.h
@@ -1,35 +1,36 @@
 #ifndef SKIPLIST_H
 #define SKIPLIST_H
 
+#define MAX_LEVEL 15
 #include <string>
 
 using namespace std;
 
 struct node {
     string key;
-    string val;
+    string data;
     // points to array of pointers
     node** next;
 };
 
 class SkipList {
+    private:
+        node* head_ptr;
+        int list_level;
+
     public:
         SkipList();
         ~SkipList();
-        node* init_node(string key, string val);
+        node* init_node(string key, string data);
         string report();
-        void insert();
-        void insert_data();
-        void remove();
-        void remove_data();
+        void insert(node* new_node);
+        void insert_data(string key, string data);
+        void remove(string key);
         int size();
         bool contains(string key);
         node* get_head();
         void set_head(node* head_ptr);
 
-    private:
-        node* head_ptr;
-        int list_level;
 };
 
 
